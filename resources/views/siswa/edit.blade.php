94c4384c46374099e48a15e6d094401ca5babbad
huda_aeda <ulrichstren19@gmail.com>
2020-02-15 21:01:39 +0700

    Authentifikasi

--- resources/views/siswa/edit.blade.php
+++ resources/views/siswa/edit.blade.php
@@ -1,6 +1,57 @@
 @extends('layouts.master')
 
 @section('content')
+    <div class="main">
+        <div class="main-content">
+            <div class="content-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="panel">
+                            <div class="panel-heading">
+                                <h2 class="panel-title">Edit Data Siswa</h2>
+                            </div>
+                            <div class="panel-body">
+                                <form action="/siswa/{{$siswa->id}}/update" method="POST">
+                                    {{ csrf_field() }}
+                                    <div class="form-group">
+                                    <label for="exampleInputEmail1">Nama Depan</label>
+                                    <input name="nama_depan" value="{{$siswa->nama_depan}}" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Nama Belakang</label>
+                                        <input name="nama_belakang" value="{{$siswa->nama_belakang}}" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                    </div>
+                
+                                    <div class="form-group">
+                                        <label for="exampleFormControlSelect1">Jenis Kelamin</label>
+                                        <select name="jenis_kelamin" class="form-control" id="exampleFormControlSelect1">
+                                        <option value="L" @if($siswa->jenis_kelamin == 'L') selected @endif>Laki - Laki</option>
+                                        <option value="P" @if($siswa->jenis_kelamin == 'P') selected @endif>Perempuan</option>
+                                        </select>
+                                    </div>
+                
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Agama</label>
+                                        <input name="agama" value="{{$siswa->agama}}" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                    </div>        
+                
+                                <div class="form-group">
+                                    <label for="exampleFormControlTextarea1">Alamat</label>
+                                <textarea name="alamat" class="form-control" id="exampleFormControlTextarea1" rows="3">{{$siswa->alamat}}</textarea>
+                                </div>
+                            
+                                    <button type="submit" class="btn btn-warning">Update</button>
+                                </form> 
+                            </div>
+                        </div> 
+                    </div>
+                </div>  
+            </div>
+        </div>
+    </div>
+@stop
+
+@section('content1')
         @if(session('sukses'))
         <div class="alert alert-success" role="alert">
             {{ session('sukses') }}
--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,92 +1,99 @@
 @extends('layouts.master')
 
 @section('content')
-        @if(session('sukses'))
-        <div class="alert alert-success" role="alert">
-            {{ session('sukses') }}
-          </div>
-          @endif
-        <div class="row">
-            <div class="col-md-6">
-                <h1> Data Siswa </h1>
+    <div class="main">
+        <div class="main-content">
+            <div class="content-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="panel">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Data Siswa</h3>
+                                <div class="right">
+                                    <button type="button" class="btn" data-toggle="modal" data-target="#exampleModal"><i class="lnr lnr-plus-circle"></i></button>
+                                </div>
+                            </div>
+                            <div class="panel-body">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th> Nama Depan </th>
+                                            <th> Nama Belakang </th>
+                                            <th> Jenis Kelamin </th>
+                                            <th> Agama </th>
+                                            <th> Alamat </th>
+                                            <th> Aksi </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach($data_siswa as $siswa)
+                                        <tr>
+                                            <td>{{$siswa->nama_depan}}</td>
+                                            <td>{{$siswa->nama_belakang}}</td>
+                                            <td>{{$siswa->jenis_kelamin}}</td>
+                                            <td>{{$siswa->agama}}</td>
+                                            <td>{{$siswa->alamat}}</td>
+                                            <td>
+                                                <a href="/siswa/{{$siswa->id}}/edit" class="btn btn-warning btn-sm">Edit</a>
+                                                <a href="/siswa/{{$siswa->id}}/delete" class="btn btn-danger btn-sm" onclick="return confirm('Yakin data di hapus?')">Delete</a>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-6">
-                <button type="button" class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#exampleModal">
-                    Tambah data siswa
+        </div>
+    </div>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Input Data Siswa</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
                 </button>
-            </div>
-            <table class="table table-hover">
-                <tr>
-                    <th> Nama Depan </th>
-                    <th> Nama Belakang </th>
-                    <th> Jenis Kelamin </th>
-                    <th> Agama </th>
-                    <th> Alamat </th>
-                    <th> Aksi </th>
-                </tr>
-                @foreach($data_siswa as $siswa)
-                <tr>
-                    <td>{{$siswa->nama_depan}}</td>
-                    <td>{{$siswa->nama_belakang}}</td>
-                    <td>{{$siswa->jenis_kelamin}}</td>
-                    <td>{{$siswa->agama}}</td>
-                    <td>{{$siswa->alamat}}</td>
-                    <td>
-                        <a href="/siswa/{{$siswa->id}}/edit" class="btn btn-warning btn-sm">Edit</a>
-                        <a href="/siswa/{{$siswa->id}}/delete" class="btn btn-danger btn-sm" onclick="return confirm('Yakin data di hapus?')">Delete</a>
-                    </td>
-                </tr>
-                @endforeach
-            </table>    
-        </div>
-    </div>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            </div>
-            <div class="modal-body">
-                <form action="/siswa/create" method="POST">
-                    {{ csrf_field() }}
-                    <div class="form-group">
-                      <label for="exampleInputEmail1">Nama Depan</label>
-                      <input name="nama_depan" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Nama Belakang</label>
-                        <input name="nama_belakang" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="exampleFormControlSelect1">Jenis Kelamin</label>
-                        <select name="jenis_kelamin" class="form-control" id="exampleFormControlSelect1">
-                          <option value="L">Laki - Laki</option>
-                          <option value="P">Perempuan</option>
-                        </select>
+                </div>
+                <div class="modal-body">
+                    <form action="/siswa/create" method="POST">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                          <label for="exampleInputEmail1">Nama Depan</label>
+                          <input name="nama_depan" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Nama Belakang</label>
+                            <input name="nama_belakang" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        </div>
+    
+                        <div class="form-group">
+                            <label for="exampleFormControlSelect1">Jenis Kelamin</label>
+                            <select name="jenis_kelamin" class="form-control" id="exampleFormControlSelect1">
+                              <option value="L">Laki - Laki</option>
+                              <option value="P">Perempuan</option>
+                            </select>
+                          </div>
+    
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Agama</label>
+                            <input name="agama" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        </div>        
+    
+                      <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Alamat</label>
+                        <textarea name="alamat" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                       </div>
-
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Agama</label>
-                        <input name="agama" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    </div>        
-
-                  <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Alamat</label>
-                    <textarea name="alamat" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                  </div>
+                </div>
+                        <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
             </div>
-                    <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-            </div>
-        </div>
-    </div>
-@endsection
+        </div>    
+@stop
 
